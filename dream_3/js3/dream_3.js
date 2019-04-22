ddfc055546e3ca496827c4107d1cad6d7ca1c30e
Kiran Khurana <art2khurana@gmail.com>
2019-04-22 19:32:56 -0400

    Cubemap setup
    set up cubemap for scene_3 using map from online generator. not the final cubemap intended for use.

---
+++ dream_3/js3/dream_3.js
@@ -0,0 +1,46 @@
+let scene, camera, renderer, controls;
+
+function init() {
+    //scene
+  scene = new THREE.Scene();
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+    //camera
+  camera = new THREE.PerspectiveCamera(45, width/height, 10, 25000);
+  camera.position.z = 1700;
+  camera.position.y = 200;
+  scene.add(camera);
+    //renderer
+  renderer = new THREE.WebGLRenderer({alpha: 1, antialias: true});
+  renderer.setSize(width, height);
+    //controls
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  document.body.appendChild(renderer.domElement);
+}
+
+  // cubemap
+    //CURRENT CUBEMAP MADE USING ONLINE GENERATOR (https://jonaszeitler.se/cubemap-toastmap-generator/)
+    //I intend to make my own cubemap for this project
+function map(){
+  scene.background = new THREE.CubeTextureLoader()
+    .setPath( 'bedcube/' )
+    .load( [
+      'px.png',
+      'nx.png',
+      'py.png',
+      'ny.png',
+      'pz.png',
+      'nz.png'
+    ] );
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update(); 
+  }
+
+init();
+map();
+animate();
+
