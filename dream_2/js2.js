96793bdd60941f8ac36f2fbc0903ae1c4c760110
Kiran Khurana <art2khurana@gmail.com>
2019-05-07 10:22:14 -0400

    new version upload
    interface + cubemap complete
    
    continue to work on canvas pixels and css

--- dream_2/js2.js
+++ dream_2/js2.js
@@ -1,37 +1,48 @@
+// canvas setup 
 const canvas = document.getElementById('drawing');
 const context = canvas.getContext('2d');
 //
-let width;
-let height;
+let width = window.innerWidth;
+let height = window.innerHeight;
 const scale = window.devicePixelRatio;
 //
 function setup(){
-	width = window.innerWidth;
-	height = window.innerHeight;
-	canvas.style.width = width + 'px';
+	canvas.style.width = width +'px';
 	canvas.style.height = height +'px';
 	canvas.width = width*scale;
 	canvas.height = height*scale;
 	context.scale(scale,scale);
 	}
-
-function energy(){
-    let pixels = context.getImageData(0, 0, canvas.width, canvas.height);
+		//console.log(width);
+		//console.log(height);
+// get mouse position
+function position(event) {
+        let x = event.clientX;
+       	console.log(x);
+       	energy(x);
+}
+// draw to canvas
+function energy(x){
+	let pixels = context.getImageData(0, 0, canvas.width, canvas.height);
     let pixelData = pixels.data;
-    for (let i = 0; i < pixelData.length; i += 44) {
-   		pixelData[i] = Math.floor(Math.random() * 0); // red
-    	pixelData[i + 1] = Math.floor(Math.random() * 0); // green
-        pixelData[i + 2] = Math.floor(Math.random() * 255); // blue
-        pixelData[i + 3] = 255; //alpha
-		}
+    	for (let i = 0; i < pixelData.length; i+=114) {
+        pixelData[i] = Math.floor(Math.random() * 255); // red
+        pixelData[i + 1] = Math.floor(Math.random() * 0); // green
+        pixelData[i + 2] = Math.floor(Math.random() * 0); // blue
+        pixelData[i + 3] = 255; //  
+   		}
     context.putImageData(pixels,0,0); 
-    //requestAnimationFrame(energy);
+    requestAnimationFrame(energy);
 	}
+
+	window.addEventListener('mousemove', position);
 
 setup();
 
-window.addEventListener('mousemove', energy);
+window.addEventListener('click', energy);
 window.addEventListener('resize', function() {
 	setup();
+  energy();
 	});
+
 
